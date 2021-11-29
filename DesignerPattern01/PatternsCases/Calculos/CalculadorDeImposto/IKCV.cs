a3e427eeedaced61c79e66916f4c7e01a09c5ae8
maanoel <manoelvitorbrito@gmail.com>
2021-11-28 23:20:18 -0300

    template method

---
+++ DesignerPattern01/PatternsCases/Calculos/CalculadorDeImposto/IKCV.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignerPattern01.PatternsCases.Calculos.CalculadorDeImposto
+{
+	public class IKCV : TemplateDeImpostoCondicional
+	{
+		public override bool DeveUsarMaximaTaxacao(Orcamento orcamento)
+		{
+			return orcamento.Valor > 500 && TemItemMaiorQue100Reais(orcamento);
+		}
+
+		public override double MaximaTaxacao(Orcamento orcamento)
+		{
+			return orcamento.Valor * 0.1;
+		}
+
+		public override double MinimaTaxacao(Orcamento orcamento)
+		{
+			throw new NotImplementedException();
+		}
+
+		private bool TemItemMaiorQue100Reais(Orcamento orcamento)
+		{
+			throw new NotImplementedException();
+		}
+
+	}
+}
---
+++ DesignerPattern01/PatternsCases/Calculos/CalculadorDeImposto/TemplateDeImpostoCondicional.cs
@@ -0,0 +1,18 @@
+﻿namespace DesignerPattern01.PatternsCases.Calculos.CalculadorDeImposto
+{
+	public abstract class TemplateDeImpostoCondicional : Imposto
+	{
+		public double Calcular(Orcamento orcamento)
+		{
+			if(DeveUsarMaximaTaxacao(orcamento)) {
+				return MaximaTaxacao(orcamento);
+			}
+
+			return MinimaTaxacao(orcamento);
+		}
+
+		public abstract bool DeveUsarMaximaTaxacao(Orcamento orcamento);
+		public abstract double MaximaTaxacao(Orcamento orcamento);
+		public abstract double MinimaTaxacao(Orcamento orcamento);
+	}
+}
