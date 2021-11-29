7845cdbc9510d12febb9058bec68d868ea2b0576
maanoel <manoelvitorbrito@gmail.com>
2021-11-28 22:14:09 -0300

    chain of responsability

---
+++ DesignerPattern01/Strategy/Calculos/CalculadorDeDescontos/DescontoVendaCasada.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignerPattern01.Strategy.Calculos.CalculadorDeDescontos
+{
+	public class DescontoVendaCasada : IDesconto
+	{
+    public IDesconto Proximo { get; set; }
+    public double Desconta(Orcamento orcamento)
+    {
+      if(aconteceuVendaCasadaEm(orcamento)) return orcamento.Valor * 0.05;
+      else return Proximo.Desconta(orcamento);
+    }
+
+    private bool aconteceuVendaCasadaEm(Orcamento orcamento)
+    {
+      return Existe("CANETA", orcamento) && Existe("LAPIS", orcamento);
+    }
+
+    private bool Existe(String nomeDoItem, Orcamento orcamento)
+    {
+      foreach(Item item in orcamento.Itens)
+      {
+        if(item.Nome.Equals(nomeDoItem)) return true;
+      }
+      return false;
+    }
+  }
+}
+}
